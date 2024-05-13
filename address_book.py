c9f1ba1f99c15661edfdfc8b48dfcaca04f47505
Serhii Zhukov <sazhukov@gmail.com>
2024-05-13 23:38:23 +0300

    1

---
+++ address_book.py
@@ -0,0 +1,45 @@
+from collections import UserDict
+import re
+
+class Field:
+    def __init__(self, value):
+        self.value = value
+
+    def __str__(self):
+        return str(self.value)
+
+class Name(Field):
+    def __init__(self, value):
+         super().__init__(value)
+
+class Phone(Field):
+    def __init__(self, value):
+        if not re.match(r'^\d{10}$', value):
+            raise ValueError("Phone number must be 10 digits")
+        super().__init__(value)
+
+class Record:
+    def __init__(self, name):
+        self.name = Name(name)
+        self.phones = []
+
+    def add_phone(self, phone):
+        self.phones.append(Phone(phone))
+
+    def __str__(self):
+        return f"Contact name: {self.name.value}, phones: {'; '.join(p.value for p in self.phones)}"
+
+class AddressBook(UserDict):
+    def add_record(self, record):
+        self.data[record.name.value] = record
+        
+    def find(self, name):
+        return self.data.get(name)
+    
+    def delete(self, name):
+        if name in self.data:
+            del self.data[name]
+    
+    
+        
+		
